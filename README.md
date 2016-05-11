1f48770683fa2203e941cf1802ba24f0e71910b1
Shawn Marincas <smarincas@thinaire.net>
2016-05-11 16:09:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# temp-log.io
+## A prototype ad-hoc logger service
+
+### Installation
+
+```
+> git clone [ repo url ]
+> cd [ repo folder ]
+> npm install
+> npm start
+```
+
+### Useage
+
+1.  Go to localhost:5000 to see URL creation form
+1.  Click Create button to get a new `/i/:uuid` URL
+1.  Send requests to URL
+1.  Go to `/o/:uuid` to see current log output
+
+*Note*: Prototype uses in-memory data storage, so restarting the app will clear all URLs and logs
+
+### To Do
+
+* Output app should look like a log terminal and should use web sockets to receive new log pushes in realtime
+* Logs should be stored in a persistent data structure
+* Form should take an optional string expression for processing log output
+* Form should take an optional number value for the limit on history
+
+### License
+
+MIT
---
+++ index.js
@@ -0,0 +1,96 @@
+"use strict";
+
+global.__require = function(name) {
+	return require(__dirname + "/" + name);
+};
+
+const Promise = require("bluebird"),
+	crypto = require("crypto"),
+	express = require("express"),
+	bodyParser = require("body-parser"),
+	expressions = require("angular-expressions"),
+	lodash = require("lodash"),
+	app = express(),
+	port = 5000,
+	expr_default = "({{ time.toLocaleString('en-us') }}): {{ method }} - {{ url }}";
+
+var logs = {};
+
+app.set("port", port);
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+	// Deliver HTML logger creation form here
+	res.sendFile("form.html", { root: __dirname + '/public' });
+});
+
+app.post("/", function(req,res) {
+	const hash = crypto.randomBytes(32).toString('hex'),
+		url = req.protocol+"://"+req.hostname+((port!=80 && port!=443) ? ":"+port : "") + "/i/" + hash,
+		logger = {
+			hash: hash,
+			url: url,
+			expr: req.body.expr || expr_default,
+			logs: [],
+			limit: 10
+		};
+
+	res.send(url);
+
+	logs[hash] = logger;
+});
+
+app.all("/i/:hash", function(req,res,next) {
+	console.log("we get here??", req.params.hash);
+
+	const logger = logs[req.params.hash];
+	if(!logger) {
+		return next();
+	}
+
+	req.time = new Date();
+
+	const scope = lodash.pick(req, "time", "method", "url", "originalUrl", "body", "headers", "query", "params"),
+		log = logger.expr.replace(/\{\{([^\}]+)\}\}/g, function(match,sub,offset,full) {
+				return expressions.compile(sub)(scope);
+			});
+
+	logger.logs.push(log);
+	res.status(200).send(log);
+
+	while(logger.logs.length>logger.limit) {
+		logger.logs.shift();
+	}
+});
+
+app.get("/o/:hash", function(req,res,next) {
+	// Deliver HTML logger reading app here
+	const logger = logs[req.params.hash];
+	if(!logger) {
+		return next();
+	}
+
+	if(req.xhr) {
+		res.json(logger.logs);
+	} else {
+		res.sendFile("output.html", { root: __dirname + '/public' });	
+	}
+});
+
+app.all("*", function expressCatchAllHandler(req,res) {
+	res.status(404).send("Not Found");
+});
+
+app.use(function expressErrorHandler(err, req, res, next) {
+	if(!err.status) {
+		console.error(err.stack);
+	}
+
+	res.status(err.status || 500).send(err.message);
+});
+
+app.listen(app.get("port"), function expressAppListenHandler() {
+	console.log("");
+	console.log("Service running on port: " + app.get("port"));
+	console.log("");
+});
